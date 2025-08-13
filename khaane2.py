3b2c349b3037a1e0ca7acc97d01b2cfbabe80d8b
mana9m <mana95m95@gmail.com>
2025-08-13 23:00:46 +0330

    Replace ـ with _

--- khaane2.py
+++ khaane2.py
@@ -3,16 +3,16 @@
 temperature = int(input("What is the temperature? "))
 window = "whatever"
 
-coolingـsystem = "off"
-heatingـsystem = "off"
+cooling_system = "off"
+heating_system = "off"
 
 if temperature < 18:
-    heatingـsystem = "on"
+    heating_system = "on"
     print("close the window. ")
     window = "close"
 
 if temperature > 25:
-    coolingـsystem = "on"
+    cooling_system = "on"
     print("close the window. ")
     window = "close"
 
@@ -23,5 +23,5 @@ if 25 > temperature > 18:
 
 print("\n\ntemperature = " + str(temperature))
 print("window = " + str(window))
-print("coolingـsystem = " + str(coolingـsystem))
-print("heatingـsystem = " + str(heatingـsystem))
+print("cooling_system = " + str(cooling_system))
+print("heating_system = " + str(heating_system))
