6fc40b0b50804a17e34c44f7d12df3ac8cf89999
mana9m <mana95m95@gmail.com>
2025-08-13 22:40:17 +0330

    Remove spaces

--- cooler.py
+++ cooler.py
@@ -3,10 +3,10 @@
 dama = int(input("damaye havaa be C chande? "))
 
 if dama > 40:
-    print("daraje cooler baayad kheyli tond baashad. ")
+    print("daraje cooler baayad kheyli tond baashad.")
 elif dama > 30:
-    print("daraje cooler baayad tond baashad. ")
+    print("daraje cooler baayad tond baashad.")
 elif dama > 24:
-    print("daraje cooler baayad kond baashad. ")
+    print("daraje cooler baayad kond baashad.")
 else:
-    print("cooler baayad khamoosh shavad. ")
+    print("cooler baayad khamoosh shavad.")
